8856a4a7b3720c5c3d25cce0590e9094e4e612f0
Alper Kaan Şimşek <47521893+alperkaansimsek@users.noreply.github.com>
2023-03-22 14:52:26 +0300

    Update math_functions.py

--- math_functions.py
+++ math_functions.py
@@ -6,9 +6,33 @@ def isPrime(number):
         if(number % i == 0):
             isPrime = False
             break
-
+'''
+#Use here if you need it. 
 if(isPrime == True):
     print("number is prime")
 else:
     print("number is not prime")
+'''
 ###################### Prime Number Checker ##############################
+
+###################### Collatz Number ##############################
+def collatz(number):
+    counter = 1 #This variable counts how many steps are needed to reach 1
+    while (number != 1):
+        if(number % 2 == 0):
+            number = number / 2
+        else:
+            number = number * 3 + 1
+        counter += 1
+    return number
+
+'''
+#Use here if you need it. 
+startNum = int(input())
+isCollatz = collatz(startNum)
+if (isCollatz == 1):
+    print("this number is a collatz number.")
+else:
+    print("this number is not a collatz number.")
+'''
+###################### Collatz Number ##############################
